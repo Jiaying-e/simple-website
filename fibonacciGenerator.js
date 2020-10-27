7fe675883cfc77cdc08c549b1f3e2e23a66e605a
Jiaying-e <68321745+Jiaying-e@users.noreply.github.com>
2020-10-27 20:26:13 +1000

    Add files via upload

---
+++ fibonacciGenerator.js
@@ -0,0 +1,32 @@
+function fibonacciGenerator (n) {
+//Do NOT change any of the code above 👆
+    
+    //Write your code here:
+    var output = [];
+    var count = n;
+    if (n != 1){
+        if (n != 2) {
+            console.log()
+            output = [0, 1];
+            output.push(output[0] + output[1]);
+
+        }
+        else{
+            console.log(Output[0, 1]);
+        }
+
+    }
+    else{
+        console.log (Output[0]);
+    }
+
+    
+    
+    
+    
+    
+    
+    //Return an array of fibonacci numbers starting from 0.
+    
+//Do NOT change any of the code below 👇
+}
